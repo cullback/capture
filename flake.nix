3fb30d6a55760ef576644b0b1090cba329163c1b
cullback <cullback@fastmail.com>
2026-02-07 09:54:09 -0500

    feat: add nix package for installing capture as a command
    Adds buildPythonApplication output with runtime dependencies
    (chromium, pandoc, single-file-cli) wrapped in PATH.

--- flake.nix
+++ flake.nix
@@ -3,37 +3,82 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            python312
-            uv
-            ruff
-            pyright
-            dprint
-            fd
-            ripgrep
-            nixfmt-rfc-style
-            # capture dependencies
-            single-file-cli
-            pandoc
-            chromium
-          ];
-        };
-      }
-    );
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python312;
+        in
+        {
+          capture = python.pkgs.buildPythonApplication {
+            pname = "capture";
+            version = "0.1.0";
+            src = ./.;
+            format = "pyproject";
+
+            build-system = [ python.pkgs.setuptools ];
+
+            dependencies = [ python.pkgs.pyyaml ];
+
+            doCheck = false;
+
+            makeWrapperArgs = [
+              "--prefix"
+              "PATH"
+              ":"
+              (pkgs.lib.makeBinPath [
+                pkgs.single-file-cli
+                pkgs.pandoc
+                pkgs.chromium
+              ])
+            ];
+
+            meta = {
+              description = "Capture websites as markdown";
+              mainProgram = "capture";
+            };
+          };
+          default = self.packages.${system}.capture;
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              python312
+              uv
+              ruff
+              pyright
+              dprint
+              fd
+              ripgrep
+              nixfmt-rfc-style
+              # capture dependencies
+              single-file-cli
+              pandoc
+              chromium
+            ];
+          };
+        }
+      );
+    };
 }
